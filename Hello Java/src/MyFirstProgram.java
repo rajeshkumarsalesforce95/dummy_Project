080c392e6e90e6e78d2f4b8661d9eec095a16fab
rajgoyal1095 <45995952+rajgoyal1095@users.noreply.github.com>
2018-12-19 21:51:06 +0530

    Update : Add two number

--- Hello Java/src/MyFirstProgram.java
+++ Hello Java/src/MyFirstProgram.java
@@ -1,5 +1,18 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World !!");
+        Add a=new Add();
+        a.add();
+        
     }
 }
+class Add{
+    public void add()
+    {
+        int a=20,b=40,c;
+        c=a+b;
+        system.out.println("Addition of a and b :"+c);
+    }
+
+}
+
