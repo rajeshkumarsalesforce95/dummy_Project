2278ed79897ea5b90c18b795ad255d04e2a8c49e
rajgoyal1095 <45995952+rajgoyal1095@users.noreply.github.com>
2018-12-23 12:28:23 +0530

    Add
    rhertehtg

---
+++ Hello Java/src/TestClass.java
@@ -0,0 +1,21 @@
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+
+        int MyFirstNumber=5;
+        int MySecondNumber=8;
+        int MyThirdNumber=MyFirstNumber + MySecondNumber;
+        int MyTotal=MyFirstNumber + MySecondNumber + MyThirdNumber;
+        int FisrtNumber=40;
+        int SecondNumber=30;
+        int Result=FirstNumber*SecondNumber; 
+        System.out.println(MySecondNumber);
+        System.out.println(MyFirstNumber);
+        System.out.println(MyThirdNumber);
+        System.out.println(MyTotal);
+        System.out.println(Result);
+        System.out.println(Result);
+        Ssytem.out.println("Hello Java");
+
+    }
+}
